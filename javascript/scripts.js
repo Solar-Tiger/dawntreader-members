7bc9ad286efc1a8614957c990e8db2e4edb870e6
David Singleton <davidjsingleton18@gmail.com>
2024-01-07 11:10:01 -0800

    Add value for id reference to manipulate fc member info

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,16 @@
 const memberDropdown = document.querySelector('select');
 
+const fcMemberName = document.getElementById('fc-member-name');
 const fcMemberImage = document.getElementById('fc-member-image');
+const fcMemberPortrait = document.getElementById('fc-member-portrait');
+const fcMemberAnecdote = document.getElementById('fc-member-anecdote');
+const fcMemberRace = document.getElementById('fc-member-race');
+const fcMemberPronouns = document.getElementById('fc-member-pronouns');
+const fcMemberTitle = document.getElementById('fc-member-tiel');
+const fcMemberFavJobs = document.getElementById('fc-member-favorite-jobs');
+const fcMemberLeastFavJobs = document.getElementById(
+  'fc-member-least-favorite-jobs'
+);
 
 fetch('../member-info.json')
   .then((response) => response.json())
@@ -9,8 +19,6 @@ fetch('../member-info.json')
 
     memberDropdown.addEventListener('change', () => {
       const memberIndex = memberDropdown.selectedIndex;
-
-      console.log(memberIndex);
 
       fcMemberImage.src = fcMembers[memberIndex].image;
     });
